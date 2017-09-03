0e2e6d023423d5f52020e97d80f5850188821222
Tristan JC Rouse Powderhorn <ryu.powderhorn@cottageindustries.build>
2017-09-03 02:36:44 -0500

    complete

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -2,7 +2,7 @@ var exports = module.exports = {}
 
 exports.signup = function(req, res) {
 
-   res.render('signup');
+    res.render('signup');
 
 }
 
@@ -10,3 +10,18 @@ exports.signin = function(req, res) {
     res.render('signin');
 }
 
+exports.dashboard = function(req, res) {
+
+    res.render('dashboard');
+
+}
+
+exports.logout = function(req, res) {
+
+    req.session.destroy(function(err) {
+
+        res.redirect('/');
+
+    });
+
+}
